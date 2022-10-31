1bd99f5474d9f87b3aed1100b4ceee12e523fb6d
BEK ROZ <bekrozikoff@gmail.com>
2022-10-31 21:57:18 +0500

    Initial Commit

---
+++ twitty/twittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  twittyApp.swift
+//  twitty
+//
+//  Created by BekRoz on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct twittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
